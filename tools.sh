3430a663cc597172851b18168c1f2cf7329866cc
Edin Hodzic <edin@moycode.uk>
2019-11-18 22:13:45 +0000

    added more tools

--- tools.sh
+++ tools.sh
@@ -5,7 +5,9 @@
 brew_cask_install java
 brew_install scala
 brew_install sbt
+brew_install jq
 brew_cask_install intellij-idea
 brew_cask_install docker
+brew_cask_install postman
 
 curl -s "https://get.sdkman.io" | bash

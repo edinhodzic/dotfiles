ba8855af19fa8eeeb7772f29d60932436293706e
Edin Hodzic <edin@moycode.uk>
2019-11-18 19:47:20 +0000

    added macos defaults

---
+++ macos/defaults.sh
@@ -0,0 +1,8 @@
+echo 'setting macos defaults'
+defaults write .GlobalPreferences AppleInterfaceStyle Dark
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -int 1
+defaults write com.apple.dock autohide -int 1
+
+# top right hot corner starts screensaver
+defaults write com.apple.dock wvous-tr-corner -int 5

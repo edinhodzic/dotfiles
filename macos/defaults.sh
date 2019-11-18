8bd245eed7d88ac8094daddcd230a4edac0f7124
Edin Hodzic <edin@moycode.uk>
2019-11-18 22:52:51 +0000

    added a macos trackpad default setting

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -2,6 +2,7 @@ echo 'setting macos defaults'
 defaults write .GlobalPreferences AppleInterfaceStyle Dark
 defaults write com.apple.menuextra.battery ShowPercent -string "YES"
 defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -int 1
+defaults write com.apple.AppleMultitouchTrackpad Clicking -int 1
 defaults write com.apple.dock autohide -int 1
 
 # top right hot corner starts screensaver

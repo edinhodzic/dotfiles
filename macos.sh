68d08fde26edcff34a6aa31fb5521d8c5cce2696
Edin Hodzic <edin@moycode.uk>
2019-11-24 20:36:19 +0000

    added more taskbar defautls

--- macos.sh
+++ macos.sh
@@ -9,4 +9,8 @@ defaults write com.apple.dock autohide -int 1
 # top right hot corner starts screensaver
 defaults write com.apple.dock wvous-tr-corner -int 5
 
+# show date and day of the week in task bar
+defaults write com.apple.menuextra.clock DateFormat -string "EEE d MMM  HH:mm"
+defaults write com.apple.menuextra.battery ShowPercent -string YES
+
 # TODO change iterm > profiles > colors > preset to "Pastel (Dark Background)"

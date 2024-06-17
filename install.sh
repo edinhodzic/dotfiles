e2b65faa9046a03508718ce29064a7bbca591416
Edin Hodzic <4981314+edinhodzic@users.noreply.github.com>
2024-06-17 23:33:58 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # brew and git are prerequisites
 if ! type brew &> /dev/null; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
   brew update
 fi

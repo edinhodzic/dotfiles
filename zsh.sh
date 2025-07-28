1bdcc2b79fc531fe7f17f93770189fad26ba6a38
Edin Hodzic <edin@edinhodzic.co.uk>
2025-07-28 17:22:46 +0100

    updated zsh script as zsh is now default on macos

--- zsh.sh
+++ zsh.sh
@@ -1,20 +1,39 @@
 #!/bin/sh
 
-HOMEBREW_NO_AUTO_UPDATE=1 brew install zsh 
-chsh -s /bin/zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo 'Setting up zsh and Oh My Zsh...'
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+# Install Oh My Zsh (zsh is already default on modern macOS)
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
 
-# TODO symlink own ~/.zshrc
-# TODO check plugins in ~/.zshrc
-# TODO ensure ~/.zshrc contains...
-# plugins=(
-#   git
-#   zsh-syntax-highlighting
-#   zsh-autosuggestions
-#   sublime
-#   z
-# )
-# TODO don't forget APPEND_HISTORY option
+# Install zsh plugins
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+fi
+
+# Configure .zshrc with desired plugins
+if [ -f "$HOME/.zshrc" ]; then
+  # Backup existing .zshrc
+  cp "$HOME/.zshrc" "$HOME/.zshrc.backup"
+  
+  # Update plugins line in .zshrc
+  sed -i '' 's/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions sublime z)/' "$HOME/.zshrc"
+  
+  # Add APPEND_HISTORY option if not present
+  if ! grep -q "APPEND_HISTORY" "$HOME/.zshrc"; then
+    echo "" >> "$HOME/.zshrc"
+    echo "# Append to history file" >> "$HOME/.zshrc"
+    echo "setopt APPEND_HISTORY" >> "$HOME/.zshrc"
+  fi
+
+  source ~/.zshrc
+  
+  echo 'Zsh configuration updated.'
+else
+  echo 'Warning: .zshrc not found. Oh My Zsh installation may have failed.'
+fi

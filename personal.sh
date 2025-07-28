ddb02a41a464a9dd6afa961c844316408c9a7d0d
Edin Hodzic <edin@edinhodzic.co.uk>
2025-07-28 21:01:45 +0100

    updated shell script for personal apps

--- personal.sh
+++ personal.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo ""
-echo '------------------------------------'
-echo 'Apps (1/1): Installing personal apps'
-echo '------------------------------------'
+echo '----------------------------------------'
+echo 'Personal (1/2): Installing personal apps'
+echo '----------------------------------------'
 
 HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask \
 whatsapp \
@@ -11,3 +11,28 @@ spotify \
 nordvpn \
 philips-hue-sync \
 sonos
+
+echo '-------------------------------------'
+echo 'Personal (2/2): Cloning some projects'
+echo '-------------------------------------'
+
+# Create development directory if it doesn't exist
+mkdir -p ~/dev/edinhodzic && cd ~/dev/edinhodzic
+
+# Clone private repositories
+echo 'Cloning reference projects...'
+git clone git@github.com:edinhodzic/reference-http4k-api.git
+git clone git@github.com:edinhodzic/reference-http4k-api-pact-test.git
+git clone git@github.com:edinhodzic/reference-http4k-api-perf-test.git
+git clone git@github.com:edinhodzic/reference-react-frontend.git
+
+echo 'Cloning cookiecutter templates...'
+git clone git@github.com:edinhodzic/cookiecutter-http4k-api.git
+git clone git@github.com:edinhodzic/cookiecutter-http4k-api-pact-test.git
+git clone git@github.com:edinhodzic/cookiecutter-http4k-api-perf-test.git
+git clone git@github.com:edinhodzic/cookiecutter-react-frontend.git
+
+echo ''
+echo 'NOTE: If git clone failed, ensure your SSH keys are set up:'
+echo '  ssh-keygen -t ed25519 -C "your_email@example.com"'
+echo '  cat ~/.ssh/id_ed25519.pub  # Add this to GitHub Settings > SSH Keys'

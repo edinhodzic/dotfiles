a9ba5bbf4e88d34e1cb417366f4650680f23186f
Edin Hodzic <edin@edinhodzic.co.uk>
2025-07-28 17:55:48 +0100

    spotify moved to personal

--- apps.sh
+++ apps.sh
@@ -4,7 +4,6 @@ HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask \
 slack \
 sublime-text \
 iterm2 \
-spotify \
 brave-browser \
 1password \
 #avast-security \ sophos-home instead?
--- personal.sh
+++ personal.sh
@@ -4,6 +4,7 @@ echo 'Installing personal applications...'
 
 HOMEBREW_NO_AUTO_UPDATE=1 brew install --cask \
 whatsapp \
+spotify \
 nordvpn \
 philips-hue-sync \
 sonos
